ce252be21acff3816d2a48b06d1c19999bb4d31d
Eduardo Díaz <iamedu@gmail.com>
2017-10-08 19:28:45 -0700

    Litho test

--- app/src/main/java/com/conveyor/transport/MainActivity.kt
+++ app/src/main/java/com/conveyor/transport/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val c = ComponentContext(this)
+
         val lithoView = LithoView.create(
                 this,
                 Text.create(c)
